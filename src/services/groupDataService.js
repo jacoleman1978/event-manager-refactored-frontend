c7dd13d2f905250172307c05ac1c78bb561f7525
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-07-21 16:24:17 -0500

    Added groupDataService connection to backend

--- src/services/groupDataService.js
+++ src/services/groupDataService.js
@@ -1,28 +1,45 @@
 import axios from "axios";
 
-class UserDataService {
-    static Login(data) {  
-        axios.defaults.withCredentials = true
-        return axios.post('http://localhost:3100/auth/login', data)
+class GroupDataService {
+    static NewGroup(data) {
+        axios.defaults.withCredentials = true;
+        return axios.post('http://localhost:3100/group/new', data)
     }
 
-    static Signup(data) {
-        axios.defaults.withCredentials = true
-        return axios.post('http://localhost:3100/auth/signup', data)
+    static InviteGroupMember(data, groupId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/group/${groupId}/invite`, data)
     }
 
-    static IsSignupInfoUnique(data) {
-        axios.defaults.withCredentials = true
-        return axios.post('http://localhost:3100/auth/signup/verify', data)
+    static RemoveGroupMember(data, groupId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/group/${groupId}/removemember`, data)
     }
 
-    static CheckSessionUser() {
-        return axios.get('http://localhost:3100/auth/session', {withCredentials: true})
+    static AcceptGroupInvite(data, groupId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/geoup/${groupId/acceptinvite}`, data)
     }
 
-    static Logout() {
-        return axios.get('http://localhost:3100/auth/logout', {withCredentials: true})
-    }   
+    static ChangeEditPrivilege(data, groupId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/group/${groupId}/membertype`, data)
+    }
+
+    static ChangeGroupName(data, groupId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/group/${groupId}/name`, data)
+    }
+
+    static DeleteGroup(data, groupId) {
+        axios.defaults.withCredentials = true;
+        return axios.delete(`http://localhost:3100/group/${groupId}`, data)
+    }
+
+    static GetOwnedGroups(data) {
+        axios.defaults.withCredentials = true;
+        return axios.get('http://localhost:3100/group/owner', data)
+    }
 }
 
-export default UserDataService;
+export default GroupDataService;
