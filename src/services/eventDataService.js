9e23a0ba99dd8cf6b3841a5eabac2ad6b997be99
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-07-21 16:11:50 -0500

    Added eventDataService connection to backend

---
+++ src/services/eventDataService.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+class EventDataService {
+    static AddEvent(data) {
+        axios.defaults.withCredentials = true;
+        return axios.post('http://localhost:3100/event/new', data)
+    }
+
+    static UpdateEvent(data, eventId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/event/${eventId}/update/info`, data)
+    }
+
+    static AddGroupToEvent(data, eventId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/event/${eventId}/update/addgroup`, data)
+    }
+
+    static RemoveGroupFromEvent(data, eventId) {
+        axios.defaults.withCredentials = true;
+        return axios.put(`http://localhost:3100/event/${eventId}/update/removegroup`, data)
+    }
+
+    static DeleteEvent(data, eventId) {
+        axios.defaults.withCredentials = true;
+        return axios.delete(`http://localhost:3100/event/${eventId}`, data)
+    }
+
+    static GetEvents(data) {
+        axios.defaults.withCredentials = true;
+        return axios.get('http://localhost:3100/event', data)
+    }
+
+    static GetEventById(data, eventId) {
+        axios.defaults.withCredentials = true;
+        return axios.get(`http://localhost:3100/event/${eventId}`, data)
+    }  
+}
+
+export default EventDataService;
