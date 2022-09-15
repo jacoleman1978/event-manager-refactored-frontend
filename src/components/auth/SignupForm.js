74c9fc89da66522c34a410dcd87dc34b0099d277
Jamie Coleman <j.a.coleman1978@gmail.com>
2022-09-15 15:01:53 -0500

    Redirect to Settings after signing up

--- src/components/auth/SignupForm.js
+++ src/components/auth/SignupForm.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import UserDataService from '../../services/userDataService';
-import getDefaultViewPath from './helpers/getDefaultViewPath';
 
 // Called from App.js
 const SignupForm = () => {
@@ -43,9 +42,8 @@ const SignupForm = () => {
 
                     // UserName is unique, so create user account and redirect to ...
                     UserDataService.Signup(data);
-                    let navPath = await getDefaultViewPath();
             
-                    navigate(navPath);
+                    navigate('/settings');
                 }
             });
         } else {
